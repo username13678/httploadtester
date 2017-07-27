6c655eb4df3df7d140cb7604089783a11455f0d7
username13678 <[omitted]>
2017-07-27 19:11:38 +0100

    Moves loadtester.py to main.py and introduces a LoadTester classw

--- loadtester.py
+++ loadtester.py
@@ -1,27 +1,10 @@
 """
 Implements an HTTP Loadtester.
 """
-import argparse
 
-
-
-def parse_arguments():
-    """Parses the command line arguments
-
-    Returns:
-        The parsed arguments.
-    """
-    parser = argparse.ArgumentParser(description='Loadtests a HTTP server.')
-    parser.add_argument('qps', type=int,
-                        help='number of queries per second to generate')
-    parser.add_argument('concurrent', type=int,
-                        help=('maximum number of concurrent queries to have',
-                              'in-flight'))
-    parser.add_argument('duration', type=int,
-                        help='duration of the test in seconds')
-    parser.add_argument('url', type=str,
-                        help='url to load test with HTTP GET requests')
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    parse_arguments()
+class LoadTester(object):
+  def __init__(self, qps, concurent, duration, url):
+    self.qps = qps
+    self.concurrent = concurrent
+    self.duration = duration
+    self.url = url
---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Launches the code.
+"""
+import argparse
+
+
+
+def parse_arguments():
+    """Parses the command line arguments
+
+    Returns:
+        The parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description='Loadtests a HTTP server.')
+    parser.add_argument('qps', type=int,
+                        help='number of queries per second to generate')
+    parser.add_argument('concurrent', type=int,
+                        help=('maximum number of concurrent queries to have',
+                              'in-flight'))
+    parser.add_argument('duration', type=int,
+                        help='duration of the test in seconds')
+    parser.add_argument('url', type=str,
+                        help='url to load test with HTTP GET requests')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    parse_arguments()
